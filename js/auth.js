7455518becef7ba2a21c70d049af4249dbf7c982
Yuval_Lankry <Yuvallankry3196@gmail.com>
2025-06-25 23:12:52 +0300

    update

--- js/auth.js
+++ js/auth.js
@@ -47,9 +47,36 @@ if (loginForm) {
   });
 }
 
-document.getElementById("signOutBtn")?.addEventListener("click", () => {
-  localStorage.removeItem("currentUser");
-  window.location.href = "login.html";
-});toggleMenu
+const signOutBtn = document.getElementById("signOutBtn");
+ const signOutMobile = document.getElementById("signOutBtnMobile");
+
+  if (signOutBtn) {
+    signOutBtn.addEventListener("click", () => {
+      localStorage.removeItem("currentUser");
+      window.location.href = "login.html";
+    });
+  }
+
+  if (signOutMobile) {
+    signOutMobile.addEventListener("click", () => {
+      localStorage.removeItem("currentUser");
+      window.location.href = "login.html";
+    });
+  }
+
+const hamburger = document.getElementById("hamburger");
+const mobileMenu = document.getElementById("mobileMenu");
+
+function toggleMenu() {
+  document.getElementById("mobileMenu").classList.toggle("active");
+}
+
+document.getElementById("hamburger").addEventListener("click", () => {
+  document.getElementById("mobileMenu").classList.toggle("active");
+});
+
+hamburger.addEventListener("click", () => {
+  mobileMenu.classList.toggle("active");
+});
 
 
